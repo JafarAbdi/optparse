d4637f84c2488537c291400d32c0c888df934694
danyboy1104 <danyboy1104.gmail.com>
2014-03-28 14:04:22 -0400

    Added bash completion support

--- optparse.bash
+++ optparse.bash
@@ -28,6 +28,8 @@ optparse_contractions=""
 optparse_defaults=""
 optparse_process=""
 optparse_arguments_string=""
+optparse_process_completion=""
+options=""
 
 # -----------------------------------------------------------------------------------------------------------------------------
 function optparse.throw_error(){
@@ -67,6 +69,8 @@ function optparse.define(){
                         local variable="$value"
                 elif [ "$key" = "value" ]; then
                         local val="$value"
+                elif [ "$key" = "list" ]; then
+                        local list="$value"
                 fi
         done
 
@@ -79,7 +83,7 @@ function optparse.define(){
         fi
 
         # build OPTIONS and help
-		optparse_usage="${optparse_usage}#NL#TB${short} $(printf "%-25s %s" "${long}:" "${desc}")"
+        optparse_usage="${optparse_usage}#NL#TB${short} $(printf "%-25s %s" "${long}:" "${desc}")"
         if [ "$default" != "" ]; then
                 optparse_usage="${optparse_usage} [default:$default]"
         fi
@@ -92,11 +96,22 @@ function optparse.define(){
                 optparse_arguments_string="${optparse_arguments_string}:"
         fi
         optparse_process="${optparse_process}#NL#TB#TB${shortname})#NL#TB#TB#TB${variable}=\"$val\";;"
+
+        # Complete options
+        options="${options} ${long}"
+        # Complete command arguments
+        if [ "$list" != "" ]; then
+                optparse_process_completion="${optparse_process_completion}#NL#TB#TB--${variable})#NL#TB#TB#TB${variable}_list=\"$list\"#NL#TB#TB#TBCOMPREPLY=( \$(compgen -W \"\${${variable}_list}\" -- \${cur}) )#NL#TB#TB#TBreturn 0;;"
+        fi
+
 }
 
 # -----------------------------------------------------------------------------------------------------------------------------
 function optparse.build(){
+        local script=${1:?}
         local build_file="$(mktemp -t "optparse-XXXXXX.tmp")"
+        local completion_dir="$HOME/.bash_completion.d/"
+        local completion_file="${completion_dir}${script}"
 
         # Building getopts header here
 
@@ -159,10 +174,34 @@ rm $build_file
 
 EOF
 
+# Create completion script
+mkdir -p $completion_dir
+        cat << EOF > $completion_file
+_$script(){
+        local cur prev options
+        COMPREPLY=()
+        cur=\${COMP_WORDS[COMP_CWORD]}
+        prev=\${COMP_WORDS[COMP_CWORD-1]}
+
+        # The basic options we'll complete.
+        options="${options}"
+
+        # Complete the arguments to some of the basic commands.
+        case \$prev in
+                $optparse_process_completion
+                *)
+        esac
+        COMPREPLY=(\$(compgen -W "\${options}" -- \${cur}))
+        return 0
+}
+complete -F _$script $script
+EOF
+
         local -A o=( ['#NL']='\n' ['#TB']='\t' )
 
         for i in "${!o[@]}"; do
                 sed -i "s/${i}/${o[$i]}/g" $build_file
+                sed -i "s/${i}/${o[$i]}/g" $completion_file
         done
 
         # Unset global variables
@@ -171,8 +210,6 @@ EOF
         unset optparse_arguments_string
         unset optparse_defaults
         unset optparse_contractions
-
-        # Return file name to parent
-        echo "$build_file"
+        unset options
 }
 # -----------------------------------------------------------------------------------------------------------------------------
